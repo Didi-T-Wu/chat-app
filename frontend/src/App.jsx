665a43e7e01c6bb59ff4c9cc5a1faf0d98cfc26e
Tichin Wu <twu38@mail.ccsf.edu>
2025-03-05 01:39:08 -0800

    Converted message-app(create-react-app) to chat-app(vite successfully) nut still have repeated login behavior

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import './App.css'
+
+import Chat from './components/Chat'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Home from './components/Home'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React + Flask SocketIO</h1>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App

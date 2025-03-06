cc28aa082b1e19c12d8f03493427e6702ff33069
Tichin Wu <twu38@mail.ccsf.edu>
2025-03-06 00:33:59 -0800

    Install chakra css library

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,7 +1,7 @@
-
+import { Button, HStack } from "@chakra-ui/react"
 import React from 'react';
 
 const Home = () =>{
-  return <div>home</div>
+  return <div><HStack><Button size="lg" colorPalette="teal" variant="solid">home</Button></HStack></div>
 }
 export default Home
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { Provider as CSSProvider} from "./components/ui/provider"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+   <CSSProvider>
     <AuthProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </AuthProvider>
+   </CSSProvider>
   </StrictMode>,
 )

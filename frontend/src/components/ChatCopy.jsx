0ac69640148fd10baff99a3e4b6d656ee1d996f1
Tichin Wu <twu38@mail.ccsf.edu>
2025-03-05 22:39:32 -0800

    Save it as a copy of my original code before use useRef to prevent extra re-render in Chat.jsx

---
+++ frontend/src/components/ChatCopy.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect, useContext, useRef } from "react";
+import { io } from "socket.io-client";
+import { useNavigate } from 'react-router-dom'
+import { API_BASE_URL } from '../config';
+import { AuthContext } from "../context/AuthContext";
+
+
+// TODO: Add timestamp to the messages
+// handle multiple users, curUser, aware that users and token stored in localStorage
+// curUser stored in sessionStorage
+// users: { username: token ...} , curUser: username, token:`token_${username}`:token
+// TODO: handle logout in user_left
+const Chat = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [socket, setSocket] = useState(null);
+  const navigate = useNavigate();
+  const {curUser, getCurUserToken} = useContext(AuthContext)
+
+  useEffect(() => {
+    //load curUser(username) and token'
+    const curUserToken = getCurUserToken(curUser)
+
+    console.log('token from localStorage in useEffect in Chat.js', curUserToken)
+    if(!curUserToken){
+      console.log('no token')
+      navigate('/login') // Redirect to login if token is missing
+      return;
+    }
+
+    // Initialize socket connection
+    // Avoid multiple socket connections
+    // TODO: but I want to handle multiple users from different tabs
+    if (!socket) {
+      console.log('Initialize socket connection in useEffect in Chat.js')
+      const newSocket = io(API_BASE_URL,{
+      query: { token:curUserToken }}); // Connect to the backend
+
+      // Handle incoming messages
+      newSocket.on("new_message", (data) => {
+        setMessages((prevMessages) => [...prevMessages, data]);
+      });
+
+      console.log('before user_joined socket state', socket)
+      newSocket.on("user_joined", (data) => {
+        console.log("Received data:", data);
+        setMessages((prevMessages) => [...prevMessages, data]);
+      });
+
+      newSocket.on("user_left", (data) => {
+        setMessages((prevMessages) => [...prevMessages, data]);
+      });
+
+      newSocket.on("auth_error", (err) => {
+        // TODO: make it a flash or warning
+        console.error(err)
+        navigate("/login");
+      });
+
+      setSocket(newSocket);
+    }
+
+    return () => {
+      console.log('Disconnecting socket in useEffect in Chat.js')
+      if (socket) socket.disconnect();
+    };
+  }, [navigate, curUser, getCurUserToken]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message.trim() && socket) {
+      socket.emit('message',{ 'msg':message, 'username':curUser }); // Emit a message event
+      setMessage("");
+    }
+  };
+
+  return (
+    <div>
+      <h2>Chat</h2>
+        <div style={{ border: "1px solid black", padding: "10px", height: "200px", overflowY: "scroll" }}>
+        {messages.map((data, index) =>{
+          if(data.system){
+            return <strong key={index}><p >{data.msg}</p></strong>
+          }else{
+            return <p key={index}><strong>{data.username}</strong> says : {data.msg}</p>
+          }})}
+        </div>
+        <form onSubmit={sendMessage}>
+         <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type a message..."
+         />
+         <button
+          type="submit"
+          disabled={!message.trim()}
+          >
+            Send
+          </button>
+        </form>
+    </div>
+  );
+};
+
+export default Chat;

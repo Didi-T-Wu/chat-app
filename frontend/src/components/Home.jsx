94bf500820b0045724235e74e2df0876b99e5d90
Tichin Wu <twu38@mail.ccsf.edu>
2025-03-23 18:06:26 -0700

    Chore: clean up some code for testing

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,16 +1,16 @@
 
 import React from 'react'
-import {  Link } from 'react-router-dom'
 
 const Home = () => {
+
+
 
 
   return (
     <div>
       <h1>Home</h1>
-      <Link to='/login'> to login</Link>
-    </div>
-  )
+    </div>)
 }
+
 
 export default Home

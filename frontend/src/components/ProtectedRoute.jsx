8eb73e5e42613ace420f4b6370779e4c59146bfc
Tichin Wu <twu38@mail.ccsf.edu>
2025-07-06 18:29:41 -0700

    Create ProtectedRoute

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,13 @@
+ import { useContext } from "react";
+ import { AuthContext } from "../context/AuthContext";
+ import { Navigate } from 'react-router-dom';
+
+
+ const ProtectedRoute = ({ children }) => {
+
+    const { isAuthenticated } = useContext(AuthContext);
+    console.log('in ProtectedRoute isAuthenticated', isAuthenticated)
+    return isAuthenticated? children: <Navigate to="/login" replace />
+ }
+
+ export default ProtectedRoute

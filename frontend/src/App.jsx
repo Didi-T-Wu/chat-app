f0ddcdddf83cc5c2dabd9f05ffaa3305a38c825b
Tichin Wu <twu38@mail.ccsf.edu>
2025-07-06 18:26:57 -0700

    Add protected route before accessing to Chat, remove home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
 
-
 import Chat from './components/Chat'
 import Signup from './pages/Signup'
-import Home from './components/Home'
 import Login from './pages/Login'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/chat" element={<Chat />} />
+        <Route path="/chat" element={
+          <ProtectedRoute>
+            <Chat />
+          </ProtectedRoute>
+          } />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<Home />} />
       </Routes>
     </div>
   )

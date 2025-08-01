a27764bbbceebc710b1a51a3dc36a04187e44f83
Tichin Wu <twu38@mail.ccsf.edu>
2025-07-31 17:25:19 -0700

    remove import index.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { Provider as CSSProvider} from "./components/common/provider.jsx"
-import './index.css'
+
 
 
 createRoot(document.getElementById('root')).render(
